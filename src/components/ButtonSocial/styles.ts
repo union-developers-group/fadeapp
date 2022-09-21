09ed8e8b292b352a30db45262b18372fbb34f94c
José Ronaldo <dev.joseronaldo@gmail.com>
2022-09-21 16:20:36 -0300

    feat: add ButtonSocial component

--- src/components/ButtonSocial/styles.ts
+++ src/components/ButtonSocial/styles.ts
@@ -5,6 +5,7 @@ items-center
 gap-4
 `
 export const ImgBoxStyle = `
+relative
 flex 
 h-16 
 w-16 
@@ -12,12 +13,32 @@ items-center
 justify-center 
 rounded-full 
 bg-primary 
-transition 
-ease-in-out 
-hover:bg-primary/[0.6]
+
+transition-all
+duration-300
+ease-in-out
+
+hover:scale-105
+
+before:absolute
+before:rounded-full
+before:content-['']
+before:w-0
+before:h-0
+before:transition-all
+before:ease-in-out
+before:duration-300
+
+before:bg-black/30
+
+hover:before:w-full
+hover:before:h-full
+hover:before:border-2
+hover:before:border-primary
 `
 export const ImgStyle = `
 max-w-[2.625rem]
+h-auto
 `
 export const TextStyle = `
 text-sm 
