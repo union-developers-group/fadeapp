0d706d3d4df0db3288cb7388e497cc131b258777
José Ronaldo <dev.joseronaldo@gmail.com>
2022-09-24 19:41:21 -0300

    test: add missing tests on Logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -21,6 +21,9 @@ describe('<Logo />', () => {
     expect(path1).not.toHaveClass('hidden')
     expect(path2).not.toHaveClass('hidden')
     expect(path3).not.toHaveClass('hidden')
+    expect(path1).toHaveAttribute('fill', '#ffffff')
+    expect(path2).toHaveAttribute('fill', '#ffffff')
+    expect(path3).toHaveAttribute('fill', '#ffffff')
   })
 
   it('should render only icon', () => {
@@ -36,6 +39,21 @@ describe('<Logo />', () => {
     expect(path1).toHaveClass('hidden')
     expect(path2).toHaveClass('hidden')
     expect(path3).toHaveClass('hidden')
+  })
+
+  it('should render dark colorText', () => {
+    const { container } = render(<Logo colorText="dark" />)
+
+    const logo = getByLabelText('Fade App')
+    const path1 = container.querySelector('svg > path:nth-child(1)')
+    const path2 = container.querySelector('svg > path:nth-child(10)')
+    const path3 = container.querySelector('svg > path:nth-child(11)')
+
+    expect(logo).toBeInTheDocument()
+
+    expect(path1).toHaveAttribute('fill', '#19223D')
+    expect(path2).toHaveAttribute('fill', '#19223D')
+    expect(path3).toHaveAttribute('fill', '#19223D')
   })
 
   it('should match snapshot', () => {
