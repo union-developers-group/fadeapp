7321b0f64320645daa797e7a420e3d5554cbecbf
matheusOliv23 <matheushenri1996@gmail.com>
2022-09-26 11:49:21 -0300

    feat: add hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,44 @@
+import { Button } from 'components/Button'
+import { Headline } from 'components/Headline'
+
+
+import {
+  ButtonContainer,
+  ButtonStyle,
+  HeroContainer,
+  Subtitle,
+  TitleContainer,
+  Try,
+} from './styles'
+
+export interface HeroProps {
+  subtitle: string
+  headlineTitle: string
+  textButton: string
+  image: string
+  tryText: string
+}
+
+export const Hero = ({
+  subtitle,
+  headlineTitle,
+  textButton,
+  tryText,
+  image,
+}: HeroProps) => {
+  return (
+    <div className={HeroContainer} style={{ backgroundImage: `url(${image})` }}>
+      <div className={TitleContainer}>
+        <Headline title={headlineTitle} />
+
+        <p className={Subtitle}>{subtitle}</p>
+
+        <div className={ButtonContainer}>
+          <Button className={ButtonStyle}>{textButton}</Button>
+
+          <p className={Try}>{tryText}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
