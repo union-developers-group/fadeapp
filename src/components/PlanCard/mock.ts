2a6cd1437bf882079f74eb931d186bf2218c900a
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-09-27 19:52:06 -0300

    feat: add PlanCard component

---
+++ src/components/PlanCard/mock.ts
@@ -0,0 +1,30 @@
+import { PlanCardProps } from '.'
+export const plans = [
+  {
+    planType: 'Semanal',
+    price: 9.99,
+    image:
+      'https://res.cloudinary.com/dbnq26wqe/image/upload/v1663434200/others/image_2_mgwhkt.jpg',
+    buttonText: 'Assine',
+  },
+  {
+    planType: 'Mensal',
+    price: 22.99,
+    haveOffer: true,
+    equivalentValue: 5.75,
+    offer: 54,
+    image:
+      'https://res.cloudinary.com/dbnq26wqe/image/upload/v1663434200/others/image_3_zuude8.jpg',
+    buttonText: 'Assine',
+  },
+  {
+    planType: 'Anual',
+    price: 239.88,
+    haveOffer: true,
+    offer: 13,
+    equivalentValue: 19.99,
+    image:
+      'https://res.cloudinary.com/dbnq26wqe/image/upload/v1663434202/others/image_4_fcsoit.jpg',
+    buttonText: 'Assine',
+  },
+] as PlanCardProps[]
