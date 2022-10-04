cd6ab07d58d0678f8fe6f9bce0fdd2d5dd446dc4
matheusOliv23 <matheushenri1996@gmail.com>
2022-10-04 19:49:42 -0300

    feat: add placeholder to next image

--- src/components/TestimonialCard/index.tsx
+++ src/components/TestimonialCard/index.tsx
@@ -11,14 +11,17 @@ export interface TestimonialCardProps {
 export const TestimonialCard = ({ name, message, image }: TestimonialCardProps) => {
   return (
     <div className={testimonialCardStyle}>
-      <h3 className={nameStyle}>
-        {name}
-      </h3>
-      <p className={messageStyle}>
-        {message}
-      </p>
+      <h3 className={nameStyle}>{name}</h3>
+      <p className={messageStyle}>{message}</p>
       <div className={photoContainerStyle}>
-        <Image alt={name} src={image} layout="fill" className="rounded-10" />
+        <Image
+          placeholder="blur"
+          blurDataURL="https://image-component.nextjs.gallery/placeholder"
+          alt={name}
+          src={image}
+          layout="fill"
+          className="rounded-10"
+        />
       </div>
     </div>
   )
