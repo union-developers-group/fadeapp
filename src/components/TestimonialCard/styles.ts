d236e1ce718e4a94410b0eebb5154242b1d5a3e9
matheusOliv23 <matheushenri1996@gmail.com>
2022-10-04 19:51:01 -0300

    fix: adjusting Testimonials styles

--- src/components/TestimonialCard/styles.ts
+++ src/components/TestimonialCard/styles.ts
@@ -1,7 +1,7 @@
 export const testimonialCardStyle = `
 flex
 flex-col
-lg:flex-row
+md:flex-row
 lg:w-[54.813rem]
 w-max
 relative
@@ -10,19 +10,19 @@ ml-7
 lg:ml-0
 `
 export const nameStyle = `
-lg:absolute
+md:absolute
 w-[18.75rem]
-lg:left-[1.875rem]
-lg:top-[3.438rem]
+md:left-[1.875rem]
+md:top-[3.438rem]
 text-white
 break-words
 overflow-hidden
 max-h-8
 mb-4
-lg:mb-0
+md:mb-0
 text-center
 font-medium
-lg:text-left
+md:text-left
 text-2xl
 box-border
 `
@@ -34,13 +34,13 @@ overflow-hidden
 max-h-[160px]
 text-center
 mb-8
-lg:mb-0
-lg:text-left
+md:mb-0
+md:text-left
 relative
 text-base
-lg:text-lg
-lg:top-[6.875rem]
-lg:left-[-6.875rem]
+md:text-lg
+md:top-[6.875rem]
+md:left-[-6.875rem]
 w-[18.75rem]
 `
 
@@ -53,7 +53,7 @@ items-center
 w-[18.75rem]
 h-[17.5rem]
 rounded-10
-lg:left-[-2.125rem]
+md:left-[-2.125rem]
 shadow-[0.5rem_0.5rem_0rem_0rem_rgba(33,231,182)]
 
 
@@ -64,18 +64,18 @@ after:h-[0.25rem]
 after:w-[1.875rem]
 after:top-[11.25rem]
 after:right-[-1.875rem]
-lg:after:w-[10.75rem]
-lg:after:top-[5.625rem]
-lg:after:right-[-10.75rem]
+md:after:w-[10.75rem]
+md:after:top-[5.625rem]
+md:after:right-[-10.75rem]
 after:bg-primary
 
 
 before:content-[' ']
 before:block
 before:absolute
-lg:before:h-[0.25rem]
-lg:before:w-[25.313rem]
-lg:before:top-[5.625rem]
-lg:before:left-[-25.313rem]
+md:before:h-[0.25rem]
+md:before:w-[25.313rem]
+md:before:top-[5.625rem]
+md:before:left-[-25.313rem]
 before:bg-primary
 `
